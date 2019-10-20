6a4fabab4b2e814e4c7eb30c625d3ac9f5a5b6a9
Shane <shanemcneil19@gmail.com>
2019-10-19 20:23:34 -0700

    add handleGetMovie function and validateBearerToken function

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const movieData = require('./movieData.json')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('Hello, world!')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
 })
+
+function handleGetMovie(req, res) {
+    const { genre, country, avg_vote } = req.query
+    let response
+
+    if(genre) {
+        response = movieData.filter(movie =>
+            movie.genre.toLowerCase().includes(genre.toLowerCase())
+        )
+    }
+
+    if(country) {
+        response = movieData.filter(movie =>
+            movie.country.toLowerCase().includes(country.toLowerCase())
+        )
+    }
+
+    if(avg_vote) {
+        response = movieData.filter(movie =>
+            Number(movie.avg_vote) >= Number(avg_vote)
+        )
+    }
+
+    res.json(response)
+}
+
+app.get('/movie', handleGetMovie)
 
 const PORT = 8000
 
